a5cb87dc5abc9c92d528463615af546a5d44488b
Jack Marchant <jack@jackmarchant.com>
2020-04-29 11:45:36 +1000

    fix

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -29,8 +29,7 @@ config :today, TodayWeb.Endpoint,
     port: String.to_integer(System.get_env("PORT")),
     transport_options: [socket_opts: [:inet6]]
   ],
-  url: [host: System.get_env("DOMAIN")],
-  force_ssl: [hsts: true],
+  url: [host: System.get_env("DOMAIN"), port: 80],
   secret_key_base: secret_key_base,
   server: true
 

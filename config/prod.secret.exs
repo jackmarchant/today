5e13e82306137b30ad3d6614890b7b806e06edfd
Jack Marchant <jack@jackmarchant.com>
2020-04-29 11:31:40 +1000

    try https again

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -25,11 +25,11 @@ secret_key_base =
     """
 
 config :today, TodayWeb.Endpoint,
-  http: [
+  https: [
     port: String.to_integer(System.get_env("PORT") || "4000"),
     transport_options: [socket_opts: [:inet6]]
   ],
-  url: [host: System.get_env("DOMAIN"), port: 443],
+  url: [host: System.get_env("DOMAIN"), port: String.to_integer(System.get_env("PORT") || "4000")],
   force_ssl: [hsts: true],
   secret_key_base: secret_key_base,
   server: true
